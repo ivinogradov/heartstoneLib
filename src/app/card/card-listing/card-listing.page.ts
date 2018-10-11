695dfac2d94755dd0a0495b1c5872cf0c7a137f5
ivinogradov <ilya.vngrdv@gmail.com>
2018-10-11 11:40:02 -0500

    Cards retrieval
    Summary:
    
    Reviewers: tyrell, patrickcharlesmayer, gilicaspi, zerodiff, alex, thefisch63
    
    Maniphest Tasks:
    
    Reviewed By:
    
    Differential Revision:

--- src/app/card/card-listing/card-listing.page.ts
+++ src/app/card/card-listing/card-listing.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CardService } from '../shared/card.service';
 
 @Component({
   selector: 'app-card-listing',
@@ -10,12 +11,17 @@ export class CardListingPage implements OnInit {
 
   private cardDeckGroup: string;
   private cardDeck: string;
+  private cards: any[] = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cardService: CardService) { }
 
   ngOnInit() {
     this.cardDeckGroup = this.route.snapshot.paramMap.get('cardDeckGroup');
     this.cardDeck = this.route.snapshot.paramMap.get('cardDeck');
+
+    this.cardService.getCardsByDeck(this.cardDeckGroup, this.cardDeck).subscribe(
+      cards => this.cards = cards
+    );
   }
 
 }
--- src/app/card/shared/card.service.ts
+++ src/app/card/shared/card.service.ts
@@ -8,25 +8,19 @@ import {CardDeck} from './card.model';
   providedIn: 'root'
 })
 export class CardService {
-
-  private readonly cardDecks: string[] = [
-    'Druid',
-    'Mage',
-    'Warrior',
-    'Rogue',
-    'Warlock',
-    'Shaman',
-    'Priest',
-    'Hunter',
-    'Paladin'
-];
   private API_KEY = 'i0GGCH789Cmsh2bSnHM6rWvDb55Pp1qO0c7jsnL7cKfuPSFtXr';
   private HS_API_URL = 'https://omgvamp-hearthstone-v1.p.mashape.com';
+  private headers: HttpHeaders;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders({'X-Mashape-Key' : this.API_KEY});
+   }
 
   public getAllCardDecks(): Observable<CardDeck[]> {
-    const headers = new HttpHeaders({'X-Mashape-Key' : this.API_KEY});
-    return this.http.get<CardDeck[]>(`${this.HS_API_URL}/info`, { headers });
+    return this.http.get<CardDeck[]>(`${this.HS_API_URL}/info`, { headers: this.headers });
+  }
+
+  public getCardsByDeck(cardDeckGroup: string, cardDeck: string): Observable<any> {
+    return this.http.get<any>(`${this.HS_API_URL}/cards/${cardDeckGroup}/${cardDeck}`, { headers: this.headers });
   }
 }
